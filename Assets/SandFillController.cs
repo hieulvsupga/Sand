17e26346dea2c0faa746033928e4c362846548ab
hieulvsupga <hieulv@supga.net>
2026-02-24 08:53:39 +0700

    224853

--- Assets/SandFillController.cs
+++ Assets/SandFillController.cs
@@ -7,6 +7,11 @@ public class SandFillController : MonoBehaviour
     [SerializeField] private float velocityRiseSpeed = 10f;  // lên nhanh
     [SerializeField] private float velocityFallSpeed = 2f;   // về 0 chậm → chuyển mượt
 
+    [Header("Move Object")]
+    [SerializeField] private Transform target;   // object sẽ di chuyển
+    [SerializeField] private float minY = -0.77f;
+    [SerializeField] private float maxY = 0.925f;
+
     private float _fillAmount;
     private float _fillVelocity;
     private bool _isFilling;
@@ -19,14 +24,25 @@ public class SandFillController : MonoBehaviour
         {
             _fillAmount = Mathf.Clamp01(_fillAmount + fillSpeed * Time.deltaTime);
             _fillVelocity = Mathf.MoveTowards(_fillVelocity, 1f, Time.deltaTime * velocityRiseSpeed);
+           // target.gameObject.SetActive(true);  // Hiện object khi bắt đầu đổ
         }
         else
         {
             // Lerp thay vì MoveTowards → giảm chậm dần tự nhiên hơn
             _fillVelocity = Mathf.Lerp(_fillVelocity, 0f, Time.deltaTime * velocityFallSpeed);
+           // target.gameObject.SetActive(false);
         }
 
+        // Update shader
         sandMaterial.SetFloat("_FillAmount", _fillAmount);
         sandMaterial.SetFloat("_FillVelocity", _fillVelocity);
+
+        // Move object theo fillAmount
+        if (target != null)
+        {
+            Vector3 pos = target.localPosition;
+            pos.y = Mathf.Lerp(minY, maxY, _fillAmount);
+            target.localPosition = pos;
+        }
     }
 }
